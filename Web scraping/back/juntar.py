c668911ada54469bdb63dcfced03a8d3b2e56076
Rafael <122636087+Cr0nik@users.noreply.github.com>
2023-08-29 10:20:51 -0300

    Update juntar.py

--- Web scraping/back/juntar.py
+++ Web scraping/back/juntar.py
@@ -1,39 +1,73 @@
+import tkinter as tk
+from tkinter import filedialog
 import pandas as pd
 import os
 
-# Pasta onde os arquivos estão localizados
-input_folder = input("Digite o caminho que os donwloads foram feitos (com DUAS barras invertidas \\) EXEMPLO: C:\\Users\\rafael.fajardo\\Desktop\\Planilhas CUB")
+def select_input_folder():
+    global input_folder
+    input_folder = filedialog.askdirectory()
+    input_folder_label.config(text="Pasta de entrada selecionada: " + input_folder)
 
-# Pasta onde o arquivo combinado será salvo
-output_folder = input("Digite o caminho que os donwloads foram feitos (com DUAS barras invertidas \\) EXEMPLO: C:\\Users\\rafael.fajardo\\Desktop\\Planilha tratada")
+def select_output_folder():
+    global output_folder
+    output_folder = filedialog.askdirectory()
+    output_folder_label.config(text="Pasta de saída selecionada: " + output_folder)
 
-# Lista para armazenar os DataFrames de cada arquivo
-data_frames = []
+def process_excel_files():
+    global input_folder, output_folder
 
-# Planilhas ou intervalos específicos a serem lidos de cada arquivo
-sheets_to_read = ['Tabela_1', 'Tabela_3']  # Substitua pelos nomes das planilhas desejadas
+    if input_folder and output_folder:
+        data_frames = []
 
-# Loop pelos arquivos na pasta de entrada
-for filename in os.listdir(input_folder):
-    if filename.endswith('.xlsx'):  # Verifique se é um arquivo Excel
-        file_path = os.path.join(input_folder, filename)
-        for sheet_name in sheets_to_read:
-            try:
-                df = pd.read_excel(file_path, sheet_name=sheet_name)  # Carregue a planilha em um DataFrame
-                data_frames.append(df)
-            except:
-                pass
+        for filename in os.listdir(input_folder):
+            if filename.endswith('.xlsx'):
+                file_path = os.path.join(input_folder, filename)
+                try:
+                    df_table_1 = pd.read_excel(file_path, sheet_name='Tabela_1')
+                    df_table_3 = pd.read_excel(file_path, sheet_name='Tabela_3')
 
-# Combine todos os DataFrames em um único DataFrame
-combined_df = pd.concat(data_frames, ignore_index=True)
+                    # Combine os DataFrames das tabelas 1 e 3 em um único DataFrame
+                    combined_df = pd.concat([df_table_1, df_table_3], ignore_index=True)
+                    data_frames.append(combined_df)
+                except:
+                    pass
 
-# Crie a pasta de saída se ela não existir
-os.makedirs(output_folder, exist_ok=True)
+        # Combine todos os DataFrames em um único DataFrame
+        if data_frames:
+            final_df = pd.concat(data_frames, ignore_index=True)
 
-# Caminho para o arquivo combinado
-output_file_path = os.path.join(output_folder, 'arquivo_combinado.xlsx')
+            output_file_path = os.path.join(output_folder, 'arquivo_juntado.xlsx')
 
-# Salve o DataFrame combinado em um novo arquivo Excel na pasta de saída
-combined_df.to_excel(output_file_path, index=False)
+            with pd.ExcelWriter(output_file_path, engine='openpyxl') as writer:
+                final_df.to_excel(writer, index=False)
 
-print(f"Arquivo combinado salvo em {output_file_path}")
+            print(f"Arquivo juntado salvo em {output_file_path}")
+
+            input_folder = ""
+            output_folder = ""
+            input_folder_label.config(text="Nenhuma pasta de entrada selecionada")
+            output_folder_label.config(text="Nenhuma pasta de saída selecionada")
+
+            root.destroy()  # Fechar a janela após processamento
+    else:
+        print("Selecione as pastas de entrada e saída primeiro!")
+
+root = tk.Tk()
+root.title("Juntar Tabelas 1 e 3 de Arquivos Excel")
+
+input_folder = ""
+output_folder = ""
+
+input_folder_label = tk.Label(root, text="Nenhuma pasta de entrada selecionada")
+select_input_folder_button = tk.Button(root, text="Selecionar Pasta de Entrada", command=select_input_folder)
+output_folder_label = tk.Label(root, text="Nenhuma pasta de saída selecionada")
+select_output_folder_button = tk.Button(root, text="Selecionar Pasta de Saída", command=select_output_folder)
+process_button = tk.Button(root, text="Processar Arquivos Excel", command=process_excel_files)
+
+input_folder_label.pack(pady=10)
+select_input_folder_button.pack()
+output_folder_label.pack()
+select_output_folder_button.pack()
+process_button.pack()
+
+root.mainloop()
